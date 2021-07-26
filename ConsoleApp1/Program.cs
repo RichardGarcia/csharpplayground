c4d052d514bdf32e74d7bc6a590bb229344de3e9
Richard Garcia <richardgarciaph@gmail.com>
2021-07-26 14:26:00 +1200

    Bolean String contains method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -50,6 +50,11 @@ namespace ConsoleApp1
             Console.WriteLine(sayHello.ToUpper());
             Console.WriteLine(sayHello.ToLower());
 
+
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
             Console.ReadKey();
 
         }
