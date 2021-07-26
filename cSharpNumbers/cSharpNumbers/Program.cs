72618277a6e2736d7042897057656c48c62351f6
Richard Garcia <richardgarciaph@gmail.com>
2021-07-26 18:43:18 +1200

    add c# numbers exercises

---
+++ cSharpNumbers/cSharpNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c# Numbers!!");
+        }
+    }
+}
