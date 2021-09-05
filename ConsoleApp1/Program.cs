e8716d2e5ddd12427b3c83618ff43ec05f645167
Richard Garcia <richardgarciaph@gmail.com>
2021-09-05 21:15:43 +1200

    Add Salamat https://www.youtube.com/c/SDPTSolutions!!!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,21 +4,48 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Clear all!!!");
 
-            
-            
-            
 
-            
 
-            
 
+            //Call Method with Parameter(s ) / Arguments samples
+
+            Add(2,3);
+
+
+
+            Console.WriteLine("Salamat https://www.youtube.com/c/SDPTSolutions!!!");
             Console.ReadKey();
 
         }
+
+        static void Add(int Num1, int num2) {
+            int sum = Num1 + num2;
+            Console.WriteLine($"{Num1} + {num2} = {sum}");
+
+
+            newMethod();
+
+        }
+
+
+
+
+        static void newMethod()
+        {
+            Console.WriteLine("New Method");
+        }
+
+
     }
+
+
+
+
+
 }
